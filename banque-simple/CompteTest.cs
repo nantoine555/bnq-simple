ba946b0fc299b2bd61084057f6bd51feed88404e
Antoine ALBERTINI <aalbertini@eliadis.com>
2020-12-09 16:14:04 +0100

    setup & teardown

--- banque-simple/CompteTest.cs
+++ banque-simple/CompteTest.cs
@@ -4,6 +4,24 @@ namespace banque
 {
     [TestFixture]
     class CompteTest {
+
+        //L'objet référence sur lesquels on va réaliser les tests
+        Compte compteAtester;
+
+        //Prévoir la création de l'objet avant chaque test
+        [SetUp]
+        public void Setup() {
+            this.compteAtester = new Compte();
+        }
+
+        //Cleanup après chaque test
+        [TearDown]
+        public void TearDown(){
+            this.compteAtester = null;
+        }
+
+
+// Les tests...
         [Test]
         public void testFalse() {
             Assert.IsFalse(false);
